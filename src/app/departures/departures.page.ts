d591a1c3ba638218834f82b669506843d7f283ba
Benjamin Hempel <benjamin.hempel@posteo.de>
2021-09-20 17:41:51 +0200

    Add toasts for adding/removing station from favorites

--- src/app/departures/departures.page.ts
+++ src/app/departures/departures.page.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 import { MonitoredStation } from '../shared/models/monitored-station.model';
 import { DepartureMonitorService } from '../shared/services/departure-monitor.service';
@@ -12,19 +13,35 @@ import { DepartureMonitorService } from '../shared/services/departure-monitor.se
 export class DeparturesPage implements OnInit {
   monitoredStations: MonitoredStation[] = [];
 
-  constructor(private departureMonitorService: DepartureMonitorService) { }
+  constructor(private departureMonitorService: DepartureMonitorService, private toastController: ToastController) { }
 
   async ngOnInit() {
     this.monitoredStations = await this.departureMonitorService.getMonitoredStations();
   }
 
+  getCombinedStationName(monitoredStation: MonitoredStation): string {
+    return monitoredStation.station.name + ', ' + monitoredStation.station.city;
+  }
+
   async onMonitoredStationAdded(stationId: string) {
     const stationToAdd = await this.departureMonitorService.getMonitoredStation(stationId);
     this.monitoredStations.push(stationToAdd);
+
+    const toast = await this.toastController.create({
+      message: this.getCombinedStationName(stationToAdd) + ' added to favorites',
+      duration: 3000
+    });
+    toast.present();
   }
 
-  onMonitoredStationRemoved(stationId: string) {
+  async onMonitoredStationRemoved(stationId: string) {
     const index = this.monitoredStations.findIndex(monitoredStation => monitoredStation._id === stationId);
-    this.monitoredStations.splice(index, 1);
+    const removedStations = this.monitoredStations.splice(index, 1);
+
+    const toast = await this.toastController.create({
+      message: this.getCombinedStationName(removedStations[0]) + ' removed from favorites',
+      duration: 3000
+    });
+    toast.present();
   }
 }
