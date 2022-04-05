fd862d9e29ec78f9e456a0a7e24dd45cdda67f86
Benjamin Hempel <benjamin.hempel@posteo.de>
2022-04-05 21:50:33 +0200

    Auto-generated commit [ci skip]

---
+++ 9072.3874f295afc8b2bded8f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdvbetter=self.webpackChunkdvbetter||[]).push([[9072],{9072:(_,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var t=s(3150),c=s(7585),e=s(1269);const n=class{constructor(o){(0,t.r)(this,o)}render(){const o=(0,c.b)(this);return(0,t.h)(t.H,{class:(0,e.c)(this.color,{[o]:!0})},(0,t.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
