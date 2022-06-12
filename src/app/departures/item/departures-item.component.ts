deb7225fb6a6f24c7ec5145d2d3ee271d6e202c5
Benjamin Hempel <benjamin.hempel@posteo.de>
2022-06-12 17:01:06 +0200

    refactor: Move string building to separate service

--- src/app/departures/item/departures-item.component.ts
+++ src/app/departures/item/departures-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { StringService } from 'src/app/shared/services/string.service';
 import { Departure } from 'src/app/shared/models/departure.model';
 
 @Component({
@@ -10,46 +10,15 @@ import { Departure } from 'src/app/shared/models/departure.model';
 export class DeparturesItemComponent implements OnInit {
   @Input() departure: Departure;
 
-  constructor(private translateService: TranslateService) { }
+  constructor(private stringService: StringService) { }
 
   ngOnInit() {}
 
   get departureTime(): string {
-    if(this.departure.relativeArrival === 0) {
-      return this.translateService.instant('departures.arrival-times.now');
-    }
-
-    if(this.departure.relativeArrival < 0) {
-      return this.translateService.instant('departures.arrival-times.min-ago', {count: Math.abs(this.departure.relativeArrival)});
-    }
-
-    if(this.departure.relativeArrival > 30) {
-      let delimiter = ':';
-      const minutes = this.departure.arrival.getMinutes();
-
-      if(minutes < 10) {
-        delimiter = ':0';
-      }
-
-      return this.departure.arrival.getHours() + delimiter + minutes;
-    }
-
-    return this.translateService.instant('departures.arrival-times.min', {count: this.departure.relativeArrival});
+    return this.stringService.getDepartureTime(this.departure);
   }
 
   get delay(): string {
-    if(this.departure.isCancelled) {
-      return this.translateService.instant('departures.delays.cancelled');
-    }
-
-    if(this.departure.relativeDelay === 0) {
-      return this.translateService.instant('departures.delays.on-time');
-    }
-
-    if(this.departure.relativeDelay < 0) {
-      return this.translateService.instant('departures.delays.early', {count: Math.abs(this.departure.relativeDelay)});
-    }
-
-    return this.translateService.instant('departures.delays.late', {count: this.departure.relativeDelay});
+    return this.stringService.getDelay(this.departure);
   }
 }
